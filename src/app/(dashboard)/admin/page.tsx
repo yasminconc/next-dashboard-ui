d82a833065d3e9fdb9283dff7e23d2e187ed1084
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2024-08-27 09:20:38 -0300

    .

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,30 @@
+import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return(
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left*/}
+      <div className="w-full lg:w-2/3">
+        {/* UserCards*/}
+        <div className="flex gap-4 justify-between flex-wrap ">
+          <UserCard type="student"/>
+          <UserCard type="teacher"/>
+          <UserCard type="parent"/>
+          <UserCard type="staff"/>
+        </div>
+
+        {/* Middle charts*/}
+        <div className=""></div>
+
+        {/* Bottom charts*/}
+        <div className=""></div>
+
+        
+      </div>
+      {/* Right*/}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  )
 };
 
 export default AdminPage;
